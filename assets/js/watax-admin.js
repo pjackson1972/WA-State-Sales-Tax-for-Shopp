1f29afa81dd45685cec203e9f6347fcb5a04738d
gehidore <jordan@beaveris.me>
2013-07-02 12:47:43 -0700

    toggle script adjustment

--- assets/js/watax-admin.js
+++ assets/js/watax-admin.js
@@ -1,22 +1,31 @@
 jQuery(document).ready(function ($) {
 	var $taxEnable = $('#wadbt_enable_tax');
 	var $taxDisable = $('#wadbt_disable_tax');
-	var $downloadEnable = $('#wabdt_enable_downloads');
+	var $downloadEnable = $('#wadbt_enable_downloads');
 	var $downloadDisable = $('#wadbt_disable_downloads');
+	
+	toggleRadios = function () {
+		console.log('toggle');
+		if($taxDisable.attr('checked') == 'checked') {
+			$($downloadDisable).attr({
+				disabled: true
+			}).attr({
+				checked: 'checked'
+			});
+			$($downloadEnable).attr({
+				disabled: true
+			});
+		} else {
+			$($downloadDisable).removeAttr('disabled');
+			$($downloadEnable).removeAttr('disabled');
+		}
+	};
+	// on load
+	toggleRadios();
 
-	$($taxDisable).on('click', function () {
-		$($downloadDisable).attr({
-			disabled: true
-		}).attr({
-			checked: 'checked'
-		});
-		$($downloadEnable).attr({
-			disabled: true
-		});
-	});
-
-	$($taxEnable).on('click', function () {
-		$($downloadDisable).removeAttr('disabled');
-		$($downloadEnable).removeAttr('disabled');
+	// on click disable
+	$('input[type=radio]').on('change', function () {
+		console.log( 'change' );
+		toggleRadios();
 	});
 });
