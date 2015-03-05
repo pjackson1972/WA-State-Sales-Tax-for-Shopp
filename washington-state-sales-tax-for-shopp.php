893ce7686503547afc14327e2e6ccb5320d831d6
Patrick Jackson <patrick@ivycat.com>
2015-03-04 19:56:05 -0800

    Add lines and update indenting to make more readable

--- washington-state-sales-tax-for-shopp.php
+++ washington-state-sales-tax-for-shopp.php
@@ -32,247 +32,281 @@ new ShoppWATaxCalc();
 
 class ShoppWATaxCalc {
 
-	protected $dor_url;
-	protected $enabled;
+    protected $dor_url;
+    protected $enabled;
 
-	public function __construct() {
-		add_action( 'admin_menu', array( &$this, 'options_page_init' ) );
-		add_action( 'register_activaton_hook', array( &$this, 'upon_install' ) );
-		$this->dor_url = 'http://dor.wa.gov';
-		$this->enabled = get_option( 'shopp_wa_destination_tax_enabled' );
-		if ( $this->enabled['tax_toggle'] === 'enable' ) {
-			add_action( 'shopp_cart_retotal', array( &$this , 'set_taxes' ) );
-		}
-		add_action( 'admin_head', array( &$this, 'set_submenu_order' ) );
-		add_action( 'admin_enqueue_scripts', array( &$this, 'admin_script' ) );
-		// filter cart template output
-		if ( $this->enabled['downloads_toggle'] === 'enable' ) {
-			add_filter( 'shopp_themeapi_cart', array( &$this, 'downloads_tax_filter' ), 11, 4 );
-			add_action( 'wp_enqueue_scripts', array( &$this, 'force_zip_tax_script' ) );
-		}
-		// update hook
-		add_action( 'admin_init', array( &$this, 'on_upgrade' ) );
-	}
+    public function __construct() {
+        
+        add_action( 'admin_menu', array( &$this, 'options_page_init' ) );
+        
+        add_action( 'register_activaton_hook', array( &$this, 'upon_install' ) );
+        
+        $this->dor_url = 'http://dor.wa.gov';
+        
+        $this->enabled = get_option( 'shopp_wa_destination_tax_enabled' );
+        
+        if ( $this->enabled['tax_toggle'] === 'enable' ) {
+            add_action( 'shopp_cart_retotal', array( &$this , 'set_taxes' ) );
+        }
+        
+        add_action( 'admin_head', array( &$this, 'set_submenu_order' ) );
+        
+        add_action( 'admin_enqueue_scripts', array( &$this, 'admin_script' ) );
+        
+        // filter cart template output
+        if ( $this->enabled['downloads_toggle'] === 'enable' ) {
+            
+            add_filter( 'shopp_themeapi_cart', array( &$this, 'downloads_tax_filter' ), 11, 4 );
+            add_action( 'wp_enqueue_scripts', array( &$this, 'force_zip_tax_script' ) );
+        }
+        
+        // update hook
+        add_action( 'admin_init', array( &$this, 'on_upgrade' ) );
+    }
 
-	public function upon_install() {
-		$db_option = array(
-			'tax_toggle'        => 'disable',
-			'downloads_toggle'  => 'disable'
-		);
-		add_option( 'shopp_wa_destination_tax_enabled', $db_option );
+    public function upon_install() {
+        
+            $db_option = array(
+                    'tax_toggle'        => 'disable',
+                    'downloads_toggle'  => 'disable'
+            );
+            
+            add_option( 'shopp_wa_destination_tax_enabled', $db_option );
+    }
 
-	}
+    public function on_upgrade() {
+        
+        $option_name = 'shopp_wa_destination_tax_enabled';
 
-	public function on_upgrade() {
-		$option_name = 'shopp_wa_destination_tax_enabled';
-		$db_option = get_option( $option_name );
-		// if they are still using the old version
-		if ( false !== $db_option AND ! is_array( $db_option ) ) {
-			// update to new option layout preserving the settings
-			$db_option = array(
-				'tax_toggle'        => $db_option,
-				'downloads_toggle'  => 'disable'
-			);
-			update_option( $option_name, $db_option );
-		}
-	}
+        $db_option = get_option( $option_name );
 
-	// admin script
-	public function admin_script( $hook ) {
-		if ( 'setup_page_washington-taxes' == $hook ) {
-			wp_enqueue_script( 'watax_admin_js', plugins_url( 'assets/js/watax-admin.js', __FILE__ ), array( 'jquery' ) );
-		}
-	}
+        // if they are still using the old version
+        if ( false !== $db_option AND ! is_array( $db_option ) ) {
+            // update to new option layout preserving the settings
+            $db_option = array(
+                    'tax_toggle'        => $db_option,
+                    'downloads_toggle'  => 'disable'
+            );
+            update_option( $option_name, $db_option );
+        }
+    }
 
-	// create filter function
-	public function downloads_tax_filter( $result, $options, $tag, $Cart ) {
-		// if this is a downloadable make it shipped so it's taxed
+    // admin script
+    public function admin_script( $hook ) {
+        if ( 'setup_page_washington-taxes' == $hook ) {
+            wp_enqueue_script( 'watax_admin_js', plugins_url( 'assets/js/watax-admin.js', __FILE__ ), array( 'jquery' ) );
+        }
+    }
 
-		if ( 'needsshippingestimates' == $tag ) {
-			$result = true; // force enable estimates
-		}
+    // create filter function
+    public function downloads_tax_filter( $result, $options, $tag, $Cart ) {
+        // if this is a downloadable make it shipped so it's taxed
 
-		if ( 'shippingestimates' == $tag ) { // return a zip field for zip calculation
-			ob_start();
-			?>
-				<div class="ship-estimates">
-					<input type="hidden" name="shipping[country]" id="shipping-country" value="US">
-					<span>
-					<?php shopp( 'customer.shipping-postcode' ); ?>
-					</span>
-					<input type="submit" name="update" value="Estimate Shipping &amp; Taxes" class="update-button">
-				</div>
-			<?php
-			$result = ob_get_clean();
-		}
+        if ( 'needsshippingestimates' == $tag ) {
+                $result = true; // force enable estimates
+        }
 
-		return $result;
-	}
+        if ( 'shippingestimates' == $tag ) { // return a zip field for zip calculation
+            ob_start();
+            ?>
+                <div class="ship-estimates">
+                    <input type="hidden" name="shipping[country]" id="shipping-country" value="US">
+                    <span>
+                        <?php shopp( 'customer.shipping-postcode' ); ?>
+                    </span>
+                    <input type="submit" name="update" value="Estimate Shipping &amp; Taxes" class="update-button">
+                </div>
+            <?php
+            $result = ob_get_clean();
+        }
 
-	// force zip entry script
-	public function force_zip_tax_script() {
-		if ( is_cart_page() ) {
-			wp_enqueue_script( 'zip_check', plugins_url( 'assets/js/zip-required.js', __FILE__ ), array( 'jquery' ) );
-			wp_localize_script( 'zip_check', 'checkoutUrl', shopp( 'checkout', 'url', 'return=true' ) );
-		}
-	}
+        return $result;
+    }
 
-	public function set_taxes() {
-		global $Shopp;
-		$Order =& ShoppOrder();
-		$state = strtolower( $Shopp->Shopping->data->Order->Shipping->state );
-		if ( 'wa' !== $state ) return false;
+    // force zip entry script
+    public function force_zip_tax_script() {
+        if ( is_cart_page() ) {
+            wp_enqueue_script( 'zip_check', plugins_url( 'assets/js/zip-required.js', __FILE__ ), array( 'jquery' ) );
+            wp_localize_script( 'zip_check', 'checkoutUrl', shopp( 'checkout', 'url', 'return=true' ) );
+        }
+    }
 
-		$address = urlencode( $Shopp->Shopping->data->Order->Shipping->address . ' ' . $Shopp->Shopping->data->Order->Shipping->saddress );
-		$city = urlencode( $Shopp->Shopping->data->Order->Shipping->city );
-		$zip = $Shopp->Shopping->data->Order->Shipping->postcode;
-		$tax= self::getTax( $address , $city, $zip );
-		$taxrate = ( string ) $tax->attributes()->rate;
-		$location_code = ( string ) $tax->attributes()->loccode;
+    public function set_taxes() {
+        
+            global $Shopp;
+            
+            $Order =& ShoppOrder();
+            
+            $state = strtolower( $Shopp->Shopping->data->Order->Shipping->state );
+            
+            if ( 'wa' !== $state ) {
+                return false;
+            }
 
-		if ( isset( $Shopp->Order->data ) ) {
-			$Shopp->Order->data['Location Code'] = $location_code;
-		}
+            $address = urlencode( $Shopp->Shopping->data->Order->Shipping->address 
+                    . ' ' . $Shopp->Shopping->data->Order->Shipping->saddress );
+            $city = urlencode( $Shopp->Shopping->data->Order->Shipping->city );
+            $zip = $Shopp->Shopping->data->Order->Shipping->postcode;
+            $tax= self::getTax( $address , $city, $zip );
+            $taxrate = ( string ) $tax->attributes()->rate;
+            $location_code = ( string ) $tax->attributes()->loccode;
 
-		$Shopp->Order->Cart->Totals->taxrate = $taxrate;
-		$subtotal = $Shopp->Order->Cart->Totals->subtotal;
-		$shipping = $Shopp->Order->Cart->Totals->shipping;
-		if ( is_numeric( $Shopp->Order->Cart->Totals->discount ) ) {
-			$discount = $Shopp->Order->Cart->Totals->discount;
-		} else {
-			$discount = 0;
-		}
+            if ( isset( $Shopp->Order->data ) ) {
+                    $Shopp->Order->data['Location Code'] = $location_code;
+            }
 
-		// calculate taxes
-		if ( shopp_setting( 'taxes' ) === on ) {
-			// don't tax donations
-			$watax_subtotal = 0;
-			foreach ( shopp_cart_items() AS $cart_item ) {
-				if ( 'off' !== $cart_item->option->tax ) {
-					$watax_subtotal = $watax_subtotal + $cart_item->totald;
-					echo $watax_subtotal;
-					echo $cart_item->totald;
-				}
-			}
-			// include shipping in tax calc...
-			if ( shopp_setting( 'tax_shipping' ) === on ) {
-				$Shopp->Order->Cart->Totals->tax = ( $watax_subtotal + $shipping ) * $taxrate;
-			} else {
-				$Shopp->Order->Cart->Totals->tax = $watax_subtotal * $taxrate;
-			}
-			$Shopp->Order->Cart->Totals->total = $Shopp->Order->Cart->Totals->tax + $subtotal + $shipping - $discount;
+            $Shopp->Order->Cart->Totals->taxrate = $taxrate;
+            $subtotal = $Shopp->Order->Cart->Totals->subtotal;
+            $shipping = $Shopp->Order->Cart->Totals->shipping;
+            if ( is_numeric( $Shopp->Order->Cart->Totals->discount ) ) {
+                    $discount = $Shopp->Order->Cart->Totals->discount;
+            } else {
+                    $discount = 0;
+            }
 
-		} else { // no taxes
-			$Shopp->Order->Cart->Totals->total = $subtotal + $shipping - $discount;
-		}
+            // calculate taxes
+            if ( shopp_setting( 'taxes' ) === on ) {
+                // don't tax donations
+                $watax_subtotal = 0;
+                foreach ( shopp_cart_items() AS $cart_item ) {
+                    if ( 'off' !== $cart_item->option->tax ) {
+                        $watax_subtotal = $watax_subtotal + $cart_item->totald;
+                        echo $watax_subtotal;
+                        echo $cart_item->totald;
+                    }
+                }
+                
+                // include shipping in tax calc...
+                if ( shopp_setting( 'tax_shipping' ) === on ) {
+                    $Shopp->Order->Cart->Totals->tax = ( $watax_subtotal + $shipping ) * $taxrate;
+                } else {
+                    $Shopp->Order->Cart->Totals->tax = $watax_subtotal * $taxrate;
+                }
+                
+                $Shopp->Order->Cart->Totals->total = $Shopp->Order->Cart->Totals->tax + $subtotal + $shipping - $discount;
 
-	}
+            } else { // no taxes
+                $Shopp->Order->Cart->Totals->total = $subtotal + $shipping - $discount;
+            }
 
-	public function options_page_init() {
-		if ( ! current_user_can( 'administrator' ) ) return;
-			$hooks = array();
-			$hooks[] = add_submenu_page( 'shopp-settings', __( 'Washington State Taxes for Shopp' ), __( 'WA State Tax' ), 'read', 'washington-taxes', array( $this, 'option_page' ) );
+    }
 
-			foreach ( $hooks as $hook ) {
-				add_action( "admin_print_styles-{$hook}", array( $this, 'load_assets' ) );
-			}
-	}
+    public function options_page_init() {
+        if ( ! current_user_can( 'administrator' ) ) {
+            return;
+        }
+        
+        $hooks = array();
+        $hooks[] = add_submenu_page( 'shopp-settings', __( 'Washington State Taxes for Shopp' ), __( 'WA State Tax' ), 'read', 'washington-taxes', array( $this, 'option_page' ) );
 
-	public function set_submenu_order() {
-		// move WA State Tax menu under Taxes
-		global $submenu;
-		$settings = $submenu['shopp-settings'];
+        foreach ( $hooks as $hook ) {
+            add_action( "admin_print_styles-{$hook}", array( $this, 'load_assets' ) );
+        }
+    }
 
-		foreach ( $settings as $key => $menu ) {
-			if ( $menu[0] == 'Taxes' ) {
-				$key++;
-				$head = array_slice( $settings, 0, $key );
-				$tail = array_slice( $settings, $key );
-			} elseif ( $menu[0] == 'WA State Tax' ) {
-				$watax = array_slice( $settings, $key );
-			}
-		}
+    public function set_submenu_order() {
+        // move WA State Tax menu under Taxes
+        global $submenu;
+        $settings = $submenu['shopp-settings'];
 
-		foreach ( $tail as $k=>$t ) {
-			if ( $t[0] != 'WA State Tax' ) {
-				$newtail[] = $t;
-			}
-		}
-		$tail = $newtail;
+        foreach ( $settings as $key => $menu ) {
+                if ( $menu[0] == 'Taxes' ) {
+                        $key++;
+                        $head = array_slice( $settings, 0, $key );
+                        $tail = array_slice( $settings, $key );
+                } elseif ( $menu[0] == 'WA State Tax' ) {
+                        $watax = array_slice( $settings, $key );
+                }
+        }
 
-		$submenu['shopp-settings'] = array_merge( $head, array_merge( $watax, $tail ) );
-	}
+        foreach ( $tail as $k=>$t ) {
+                if ( $t[0] != 'WA State Tax' ) {
+                        $newtail[] = $t;
+                }
+        }
+        $tail = $newtail;
 
-	public function load_assets() {
+        $submenu['shopp-settings'] = array_merge( $head, array_merge( $watax, $tail ) );
+    }
 
-	}
+    public function load_assets() {
 
-	public function set_status() {
-		$option_name = 'shopp_wa_destination_tax_enabled';
-		if ( ! empty( $_POST['wadbt_status'] ) ) {
-			if ( $_POST['wadbt_status']['tax_toggle'] == 'disable' ) {
-				$_POST['wadbt_status']['downloads_toggle'] = 'disable';
-			}
-			// loop and build the array to save
-			foreach ( $_POST['wadbt_status'] AS $key => $status ) {
-				if ( $status === 'enable' ) {
-					$db_option[ $key ] = 'enable';
-				} else {
-					$db_option[ $key ] = 'disable';
-				}
-			}
-		}
-		// check for options to be saved
-		$prior_status = get_option( $option_name );
-		if ( $prior_status == $db_option ) { // already set
-			$result = true;
-		} else { // save it
-			$result = update_option( $option_name, $db_option );
-		}
-		// return the result for user notification
-		return $result;
-	}
+    }
 
-	public function option_page() {
-		if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
-			$response = self::set_status();
-		}
-		require_once 'assets/views/option-page-view.php';
-	}
+    public function set_status() {
+        $option_name = 'shopp_wa_destination_tax_enabled';
+        if ( ! empty( $_POST['wadbt_status'] ) ) {
+            if ( $_POST['wadbt_status']['tax_toggle'] == 'disable' ) {
+                    $_POST['wadbt_status']['downloads_toggle'] = 'disable';
+            }
+            // loop and build the array to save
+            foreach ( $_POST['wadbt_status'] AS $key => $status ) {
+                    if ( $status === 'enable' ) {
+                            $db_option[ $key ] = 'enable';
+                    } else {
+                            $db_option[ $key ] = 'disable';
+                    }
+            }
+        }
+        
+        // check for options to be saved
+        $prior_status = get_option( $option_name );
+        if ( $prior_status == $db_option ) { // already set
+            $result = true;
+        } else { // save it
+            $result = update_option( $option_name, $db_option );
+        }
+        
+        // return the result for user notification
+        return $result;
+    }
 
-	protected function getTax( $addr, $city, $zip ) {
-		$req = $this->dor_url . "/AddressRates.aspx?output=xml&addr=$addr&city=$city&zip=$zip";
-		return self::get_decoded_url( $req );
-	}
+    public function option_page() {
+        if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+                $response = self::set_status();
+        }
+        
+        require_once 'assets/views/option-page-view.php';
+    }
 
-	protected function get_decoded_url( $url ) {
-		$result = wp_remote_get( $url );
-		if ( ! is_wp_error( $result ) ) {
-			$xml = new SimpleXMLElement( $result['body'] );
+    protected function getTax( $addr, $city, $zip ) {
+        
+        $req = $this->dor_url . "/AddressRates.aspx?output=xml&addr=$addr&city=$city&zip=$zip";
+        
+        return self::get_decoded_url( $req );
+    }
 
-			switch( $xml->attributes()->code ) {
-				case 0:
-					// Code 0 means address was perfect
-					break;
-				case 1:
-					$xml->msg = "Warning: The address was not found, but the ZIP+4 was located.";
-					break;
-				case 2:
-					$xml->msg = "Warning: Neither the address or ZIP+4 was found, but  the 5-digit ZIP was located.";
-					break;
-				case 3:
-					$xml->msg = "Error: The address, ZIP+4, and ZIP could not be found.";
-					break;
-				case 4:
-					$xml->msg = "Error: Invalid arguements.";
-					break;
-				case 5:
-					$xml->msg = "Error: Internal error.";
-			}
-		} else {
-			$xml = "Error: Could not load XML.";
-		}
+    protected function get_decoded_url( $url ) {
 
-		return  $xml;
-	}
+        $result = wp_remote_get( $url );
+
+        if ( ! is_wp_error( $result ) ) {
+
+            $xml = new SimpleXMLElement( $result['body'] );
+
+            switch( $xml->attributes()->code ) {
+                case 0:
+                    // Code 0 means address was perfect
+                    break;
+                case 1:
+                    $xml->msg = "Warning: The address was not found, but the ZIP+4 was located.";
+                    break;
+                case 2:
+                    $xml->msg = "Warning: Neither the address or ZIP+4 was found, but  the 5-digit ZIP was located.";
+                    break;
+                case 3:
+                    $xml->msg = "Error: The address, ZIP+4, and ZIP could not be found.";
+                    break;
+                case 4:
+                    $xml->msg = "Error: Invalid arguements.";
+                    break;
+                case 5:
+                    $xml->msg = "Error: Internal error.";
+            }
+        } else {
+            $xml = "Error: Could not load XML.";
+        }
+
+        return  $xml;
+    }
 }
